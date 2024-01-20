3b4925f97245763e7ef586dbfe41bf257fad652e
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-01-20 08:46:26 +0300

    add config.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+import config
 
-# Register your models here.
+
+admin.site.site_header = config.SITE_HEADER
+admin.site.site_title = config.SITE_TITLE
+admin.site.index_title = config.INDEX_TITLE
---
+++ main_app/base.py
@@ -0,0 +1,68 @@
+import uuid
+from django.db import models
+from django.utils.dateformat import format
+
+
+class Base(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    comment = models.TextField(
+        verbose_name="Комментарий",
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания",
+        auto_now_add=True,
+        null=True,
+        blank=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата изменения",
+        auto_now=True,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Directory(Base):
+    name = models.CharField(
+        verbose_name="Наименование",
+        max_length=150,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    class Meta:
+        abstract = True
+
+
+class Document(Base):
+    number = models.IntegerField(
+        verbose_name="Номер",
+        null=True,
+        blank=True,
+        editable=False,
+        default=0
+    )
+    date = models.DateTimeField(
+        verbose_name="Дата",
+        null=True,
+        blank=True
+    )
+
+    def __str__(self) -> str:
+        return f"Документ №{self.id} от {format(self.date, '%Y.%m.%d')}"
+
+    class Meta:
+        abstract = True
