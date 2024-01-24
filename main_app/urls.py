1aee0be5d578fe15a69ed633bb2cd4483f3c6cd2
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-01-24 07:44:54 +0300

    add templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from main_app.views import IndexView, AboutView
 app_name = 'main_app'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
+    path('', IndexView.as_view(), name="show-index-page"),
     path('about/', AboutView.as_view(), name="about"),
 ]
